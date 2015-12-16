d1364374479a0d34fd849f319ad150fac4270eba
Allen Qiu <3911744@qq.com>
2015-12-16 19:37:41 +0800

    v1.1 兼容conky1.10并优化配置,优化st脚本和安装脚本

--- README.md
+++ README.md
@@ -7,18 +7,21 @@
 ## 依赖
 
 - 调用conky作为i3bar输出
+- dmenu启动程序
 - feh显示壁纸
 - xcompmgr透明插件（可选）
-- freetype-infinality字体渲染美化（可选）
+- infinality字体渲染美化（可选）
 
 ## 字体
 
-- Monaco:一个很好看的等宽英文字体，非常适用于写代码，若显示不佳请用freetype-infinality渲染
-- font-awesome:开源的图标字体，可通过简单地复制粘贴使用，具体查看：http://fortawesome.github.io/Font-Awesome/cheatsheet/
+- Monaco:一个很好看的等宽英文字体，非常适用于写代码，若显示不佳请用infinality渲染
+- font-awesome:开源的图标字体，可通过简单地复制粘贴使用，具体查看:
+http://fortawesome.github.io/Font-Awesome/cheatsheet/
 
 ## 安装
 
-先行安装好i3（一个不到1M的WM:)
+先行安装所需程序:i3 dmenu feh
+
 ```bash
 # 克隆oh-my-i3到本地
 git clone https://github.com/ID1258/oh-my-i3.git
@@ -27,7 +30,8 @@ cd oh-my-i3
 # 安装
 sh install.sh
 ```
-若已运行i3需要`$mod+Shift+R`重载入配置
+
+若已运行i3需要 `Alt+Shift+R` 重载入配置
 
 ## 快捷键
 
@@ -43,9 +47,9 @@ sh install.sh
 #!/bin/sh
 
 # 关闭嘟嘟的警告声
-xset -b off
+xset -b
 # 一段时间无操作自动关闭屏幕
-xset dpms 900
+xset dpms 600
 
 # 笔记本触摸板设置
 # TapButton定义鼠标左/中/右键分别为单指/双指/三指操作
@@ -65,9 +69,12 @@ xset dpms 900
 - dmenu样式：与ibar统一样式，字体支持，等宽覆盖。请先更新至4.6版本。
 - 图标字体：更直观更美观。
 - 智能启动：添加智能启动脚本st（虽然很笨），简化启动，避免重复运行。
+- 更多:靠你们来拯救世界了！
 
 ![2](https://github.com/JH623/screenshot/blob/master/oh-my-i3/2.png)
 
-- 更多:靠你们来拯救世界了！
+## 版本更新
+
+- 2015/12/16 v1.1 兼容conky1.10并优化配置,优化st脚本和安装脚本
 
                                                   Allen Qiu
--- install.sh
+++ install.sh
@@ -1,21 +1,34 @@
-#!/bin/sh
-cd `dirname $0`
-mkdir ~/bin ~/.fonts ~/.config/i3 > /dev/null 2>&1
-cp -uvr font-awesome Monaco.ttf ~/.fonts
-cp -iuv config conkyrc_bar ~/.config/i3/
-cp -iuv st ~/bin/
+#!/bin/bash
+dir=`dirname $0`
+conky_ver=`conky -v | grep "conky 1.10" > /dev/null && echo "1.10" || echo "1.9"`
 NET=`/sbin/ifconfig | awk '/192/{print a}{a=$1}' | sed 's/://g'`
-C=~/.config/i3/config
-sed -i "s/wlan0/$NET/g" ~/.config/i3/conkyrc_bar
-if [ "`md5sum $C|cut -d ' ' -f1`" = "`md5sum config|cut -d ' ' -f1`" ];then
+C=$HOME/.config/i3/config
+
+# 备份旧文件
+cd ~/.config/i3/ 2> /dev/null && mv config config_old 2> /dev/null && mv conkyrc_bar conkyrc_bar_old 2> /dev/null
+mv ~/bin/st ~/bin/st_old 2> /dev/null
+
+# 生成所需目录
+mkdir ~/bin ~/.fonts ~/.config/i3 2> /dev/null
+
+# 安装字体与st脚本
+cd $dir
+cp -r font-awesome Monaco.ttf ~/.fonts
+cp st ~/bin/
+
+# 生成conkyrc_bar配置
+cp conkyrc_bar${conky_ver} ~/.config/i3/conkyrc_bar
+    sed -i "s/wlan0/$NET/g" ~/.config/i3/conkyrc_bar
+
+# 生成i3配置
+cp config ~/.config/i3/
     sed -i "s/xfce4-terminal/i3-sensible-terminal/g" $C
     sed -i "17,24s/^\([^#]\)/#\1/g" $C
     sed -i "46,50s/^\([^#]\)/#\1/g" $C
     sed -i "59s/^\([^#]\)/#\1/g" $C
     sed -i "62s/^\([^#]\)/#\1/g" $C
     sed -i "23,24d" $C
-else
-    echo "\n已有的config配置比oh-my-i3新，这将不会被改动！\n如若确定需要安装，请先执行rm $C，然后再继续！"
-    exit 1
-fi
-echo "\n安装成功，请参照说明先配置好$C 以获得更好的体验！"
+
+fc-cache
+
+echo -e "\n安装成功，请参照说明先配置好 $C 并重载以获得更好的体验！"
